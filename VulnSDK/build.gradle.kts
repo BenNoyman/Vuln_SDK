25997de41dca0ba22e5975771778e15c20f0a69c
BenNoyman <ben@example.com>
2025-06-13 15:16:31 +0300

    Fix JitPack build issues: Java 17, proper Maven publishing, remove script

--- VulnSDK/build.gradle.kts
+++ VulnSDK/build.gradle.kts
@@ -27,6 +27,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
@@ -49,12 +55,38 @@ dependencies {
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("maven") {
+            create<MavenPublication>("release") {
                 from(components["release"])
                 
                 groupId = "com.github.BenNoyman"
-                artifactId = "VulnSDK"
+                artifactId = "Vuln_SDK"
                 version = "1.0.0"
+                
+                pom {
+                    name.set("VulnSDK2")
+                    description.set("A powerful Android SDK for vulnerability scanning and security monitoring")
+                    url.set("https://github.com/BenNoyman/Vuln_SDK")
+                    
+                    licenses {
+                        license {
+                            name.set("MIT License")
+                            url.set("https://opensource.org/licenses/MIT")
+                        }
+                    }
+                    
+                    developers {
+                        developer {
+                            id.set("BenNoyman")
+                            name.set("Ben Noyman")
+                        }
+                    }
+                    
+                    scm {
+                        connection.set("scm:git:git://github.com/BenNoyman/Vuln_SDK.git")
+                        developerConnection.set("scm:git:ssh://github.com:BenNoyman/Vuln_SDK.git")
+                        url.set("https://github.com/BenNoyman/Vuln_SDK")
+                    }
+                }
             }
         }
     }
